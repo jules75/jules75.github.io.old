b9f71fd4d706551d8e39ecd6250829ca7ba4a4d0
Julian Laffey <julianlaffey@gmail.com>
2014-11-28 14:44:00 +1100

    Move dictionary to separate file

--- hello_world.js
+++ hello_world.js
@@ -1,6 +1,7 @@
 goog.addDependency("base.js", ['goog'], []);
 goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
+goog.addDependency("../hello_world/data.js", ['hello_world.data'], ['cljs.core']);
 goog.addDependency("../dommy/utils.js", ['dommy.utils'], ['cljs.core']);
 goog.addDependency("../clojure/string.js", ['clojure.string'], ['goog.string', 'cljs.core', 'goog.string.StringBuffer']);
 goog.addDependency("../dommy/core.js", ['dommy.core'], ['cljs.core', 'dommy.utils', 'clojure.string']);
-goog.addDependency("../hello_world/core.js", ['hello_world.core'], ['cljs.core', 'dommy.core', 'clojure.string']);
+goog.addDependency("../hello_world/core.js", ['hello_world.core'], ['cljs.core', 'hello_world.data', 'dommy.core', 'clojure.string']);
